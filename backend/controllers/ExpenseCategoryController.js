73426f82fc158817f5ebaa1377aafa9b0ed52fb0
Bryan Henrique Cruz <candorrick@gmail.com>
2020-09-03 01:37:47 -0300

    Implementado pasta controllers no backend

---
+++ backend/controllers/ExpenseCategoryController.js
@@ -0,0 +1,75 @@
+import expenseCategoryModel from '../models/expenseCategoryModel.js';
+
+// retorna uma categoria pelo id
+const findOneExpenseCategory = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    let doc = await expenseCategoryModel.findById(id);
+    res.send({ doc: doc });
+  } catch (error) {
+    res.status(500).send({ error: error });
+  }
+};
+
+// retorna uma ou mais categorias pelo field e search
+const FindExpenseCategoryByField = async (req, res) => {
+  const { field, search } = req.params;
+  const query = { [field]: { $regex: `.*${decodeURIComponent(search)}.*` } };
+
+  try {
+    let docs = await expenseCategoryModel.find(query);
+    res.send({ docs: docs });
+  } catch (error) {
+    res.status(500).send({ error: error });
+  }
+};
+
+// atualiza uma category
+const updateExpenseCategory = async (req, res) => {
+  const { id } = req.params;
+  const docUpdate = req.body;
+
+  try {
+    let categoryUpdated = await expenseCategoryModel.findByIdAndUpdate(
+      id,
+      docUpdate
+    );
+    await categoryUpdated.save();
+    res.send(true);
+  } catch (error) {
+    res.status(500).send({ error: error });
+  }
+};
+
+// cria uma nova categoria
+const createExpenseCategory = async (req, res) => {
+  const docBody = req.body;
+
+  try {
+    let newDoc = await new expenseCategoryModel(docBody);
+    await newDoc.save();
+    res.send({ newDoc: newDoc });
+  } catch (error) {
+    res.status(500).send({ error: error });
+  }
+};
+
+// deleta uma category
+const deleteExpenseCategory = async (req, res) => {
+  const { id } = req.params;
+  try {
+    await expenseCategoryModel.findByIdAndDelete(id);
+    res.send(true);
+  } catch (error) {
+    res.status(500).send({ error: error });
+  }
+};
+
+export default {
+  findOneExpenseCategory,
+  FindExpenseCategoryByField,
+  updateExpenseCategory,
+  createExpenseCategory,
+  deleteExpenseCategory,
+};
--- backend/routes/ExpenseCategoryRouter.js
+++ backend/routes/ExpenseCategoryRouter.js
@@ -1,74 +1,24 @@
 import express from 'express';
-import expenseCategoryModel from '../models/expenseCategoryModel.js';
-import e from 'express';
+import {
+  findOneExpenseCategory,
+  FindExpenseCategoryByField,
+  updateExpenseCategory,
+  createExpenseCategory,
+  deleteExpenseCategory,
+} from '../controllers/ExpenseCategoryController.js';
 
 const router = express.Router();
 
 router.use(express.json());
 
-// retorna uma categoria pelo id
-router.get('/findone/:id', async (req, res) => {
-  const { id } = req.params;
+router.get('/findone/:id', findOneExpenseCategory());
 
-  try {
-    let doc = await expenseCategoryModel.findById(id);
-    res.send({ doc: doc });
-  } catch (error) {
-    res.status(500).send({ error: error });
-  }
-});
+router.get('/findbyfield/:field/:search', FindExpenseCategoryByField());
 
-// retorna uma ou mais categorias pelo field e search
-router.get('/findbyfield/:field/:search', async (req, res) => {
-  const { field, search } = req.params;
-  const query = { [field]: { $regex: `.*${decodeURIComponent(search)}.*` } };
+router.post('/updatecategory/:id', updateExpenseCategory());
 
-  try {
-    let docs = await expenseCategoryModel.find(query);
-    res.send({ docs: docs });
-  } catch (error) {
-    res.status(500).send({ error: error });
-  }
-});
+router.post('/newcategory', createExpenseCategory());
 
-// atualiza uma category
-router.post('/updatecategory/:id', async (req, res) => {
-  const { id } = req.params;
-  const docUpdate = req.body;
-
-  try {
-    let categoryUpdated = await expenseCategoryModel.findByIdAndUpdate(
-      id,
-      docUpdate
-    );
-    await categoryUpdated.save();
-    res.send(true);
-  } catch (error) {
-    res.status(500).send({ error: error });
-  }
-});
-
-// cria uma nova categoria
-router.post('/newcategory', async (req, res) => {
-  const docBody = req.body;
-
-  try {
-    let newDoc = await new expenseCategoryModel(docBody);
-    await newDoc.save();
-    res.send({ newDoc: newDoc });
-  } catch (error) {
-    res.status(500).send({ error: error });
-  }
-});
-
-router.delete('/deletecategory/:id', async (req, res) => {
-  const { id } = req.params;
-  try {
-    await expenseCategoryModel.findByIdAndDelete(id);
-    res.send(true);
-  } catch (error) {
-    res.status(500).send({ error: error });
-  }
-});
+router.delete('/deletecategory/:id', deleteExpenseCategory());
 
 export default router;
