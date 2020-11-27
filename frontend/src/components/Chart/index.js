3429fdbd4e3b20c3dc0d8dd47e1bc44b50813861
Bryan Cruz <bryan.oficio@gmail.com>
2020-11-26 23:05:14 -0300

    BACKEND:alterado model de transaction;FRONTEND:chart.js funcionando

--- frontend/src/components/Chart/index.js
+++ frontend/src/components/Chart/index.js
@@ -1,10 +1,23 @@
 import React, { useState, useEffect } from 'react';
-import 'chart.js';
+import Chart from 'chart.js';
 
-export default function Chart({ id, children: data }) {
+export default function ChartComponent({ id, children: data }) {
+  const [firstChart, setFirstChart] = useState(new Chart('myChart', data));
+
+  function removeData(chart) {
+    let alteredChart = new Chart('myChart',data);
+    alteredChart.data.labels.pop();
+    alteredChart.data.datasets.forEach((dataset) => {
+        dataset.data.pop();
+    });
+    alteredChart.update();
+    setFirstChart(alteredChart);
+}
+
   return (
-    <div>
+    <>
       <canvas id={id} width="400" height="400"></canvas>
-    </div>
+      <button onClick={removeData}>apagar</button>
+    </>
   );
 }
